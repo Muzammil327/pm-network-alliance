c75766ea914f185bcd619f1bc8dd763cc24b3c1c
Muzammil327 <muzammilsafdarofficial@gmail.com>
2025-02-12 16:03:13 +0500

    updated:latest

--- frontend/src/pages/AddCatgeory.jsx
+++ frontend/src/pages/AddCatgeory.jsx
@@ -11,7 +11,7 @@ const AddCatgeory = () => {
     <>
       <div className="text-center !py-20">
         <h1 className="my-3 sm:my-4 text-2xl sm:text-4xl md:text-5xl font-medium leading-tight text-white">
-          Add Catgeory
+          Add Catgeory s
         </h1>
       </div>
       <CatgeoryForm onSuccess={handleSuccess} />
